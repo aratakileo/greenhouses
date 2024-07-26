9ca246da622144170cbc9f31b9f77156f3978e6d
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2024-07-27 02:03:03 +0300

    Update GreenhouseScreenContainer.java

--- src/main/java/io/github/aratakileo/greenhouses/container/GreenhouseScreenContainer.java
+++ src/main/java/io/github/aratakileo/greenhouses/container/GreenhouseScreenContainer.java
@@ -12,7 +12,6 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 
 public class GreenhouseScreenContainer extends AbstractContainerMenu {
